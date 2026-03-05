b4d762099d33c94216c69a8c32cc2f63803cf4ab
CactusHoodie24 <preciousphimba@gmail.com>
2026-03-05 12:14:18 -0500

    Env moved into the src

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,3 @@
-require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
 const bcrypt     = require('bcryptjs');
 const jwt        = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
